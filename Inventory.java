f7b7837a686588299ae853af3b205a859b85d83d
Glurp <ktkellerbusiness@gmail.com>
2024-07-19 18:41:30 -0400

    Test

--- Inventory.java
+++ Inventory.java
@@ -1,10 +1,25 @@
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import org.apache.poi.hssf.usermodel.HSSFSheet;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.FormulaEvaluator;
+import org.apache.poi.ss.usermodel.Row;
+
 public class Inventory {
     String Status;
     int ProductID;
     String Name;
     String Vendor;
 
-    Inventory(){
+    Inventory() throws FileNotFoundException {
+        try {
+            FileInputStream in = new FileInputStream(new File("/Users/Glurp/Downloads"));
 
+
+        } catch (FileNotFoundException e){
+            System.out.println(e.getMessage());
+        }
     }
 }
