a3ee51daee4a9a5cc4b270110889006e8eb8701a
Glurp <ktkellerbusiness@gmail.com>
2024-07-19 18:52:40 -0400

    Test

--- Warehouse.java
+++ Warehouse.java
@@ -1,13 +1,15 @@
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import org.apache.poi.hssf.usermodel.HSSFSheet;
-import org.apache.poi.hssf.usermodel.HSSFWorkbook;
-import org.apache.poi.ss.usermodel.Cell;
-import org.apache.poi.ss.usermodel.FormulaEvaluator;
-import org.apache.poi.ss.usermodel.Row;
 
-public class Inventory {
+/*
+This class, as the inventory to the
+
+
+
+
+ */
+public class Warehouse {
     String Status;
     int ProductID;
     String containerName;
@@ -17,7 +19,7 @@ public class Inventory {
     int onHand;
     int reserved;
 
-    Inventory() throws FileNotFoundException {
+    Warehouse() throws FileNotFoundException {
         try {
             FileInputStream in = new FileInputStream(new File("/Users/Glurp/Downloads"));
 
