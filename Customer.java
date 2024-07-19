cad329eb8e9b5254d0c3e2283491dedc188982eb
Glurp <ktkellerbusiness@gmail.com>
2024-07-19 15:59:40 -0400

    Test

--- Customer.java
+++ Customer.java
@@ -1,2 +1,15 @@
 public class Customer {
+    String First_Name;
+    String Last_Name;
+    int Contact_num;
+    String Email;
+    String Street;
+    String State;
+    String City;
+    int Zipcode;
+    String CustomerID;
+
+    Customer(){
+
+    }
 }
