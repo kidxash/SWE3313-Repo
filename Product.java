17bf836ad5fddf8f62b0861a0e3c9841082f7969
Glurp <ktkellerbusiness@gmail.com>
2024-07-19 20:22:21 -0400

    Test

--- Product.java
+++ Product.java
@@ -1,13 +1,27 @@
+import java.text.DateFormat;
+import java.text.Format;
+import java.time.LocalDate;
+import java.util.Date;
+
 public class Product {
     String Status;
     final int ProductID;
-
+    String Description;
     String Vendor;
     int onHand;
     int reserved;
+    int sold;
+    LocalDate LastShipment;
 
 
-    Product(){
-        ProductID=0;
+    Product(String status, int productID, String description, String vendor, int onHand, int sold, LocalDate lastShipment, int reserved ){
+        ProductID=productID;
+        Status=status;
+        Description=description;
+        Vendor=vendor;
+        this.onHand=onHand;
+        this.sold=sold;
+        LastShipment=lastShipment;
+        this.reserved=reserved;
     }
 }
--- Warehouse.java
+++ Warehouse.java
@@ -1,10 +1,19 @@
+import org.apache.poi.hssf.usermodel.HSSFSheet;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.FormulaEvaluator;
+import org.apache.poi.ss.usermodel.Row;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.util.Date;
 
 /*
-This class, as the inventory to the
-
+This class, as the inventory to the warehouse, must be able to accept a large, current register
+of the warehouse after an audit, and it is used to add new products to the warehouse.
 
 
 
@@ -22,10 +31,56 @@ public class Warehouse {
     Warehouse() throws FileNotFoundException {
         try {
             FileInputStream in = new FileInputStream(new File("/Users/Glurp/Downloads"));
+            HSSFWorkbook workBook=new HSSFWorkbook(in);
+            HSSFSheet sheet=workBook.getSheetAt(0);
 
-
-        } catch (FileNotFoundException e){
+            int counter=0;
+            for(Row row: sheet)     //iteration over row using for each loop
+            {
+                String status="", description="", vendor="", String, Date="";
+                int productID=0, onHand=0,sold=0,reserved=0;
+                LocalDate shipment=LocalDate.parse("0000-00-00");
+                int c=0;
+                for (Cell cell : row)    //iteration over cell using for each loop
+                {
+                    switch (c){
+                        case 0:
+                            status= cell.getStringCellValue();
+                            break;
+                        case 1:
+                            productID=(int)cell.getNumericCellValue();
+                            break;
+                        case 2:
+                            description= cell.getStringCellValue();
+                            break;
+                        case 3:
+                            vendor=cell.getStringCellValue();
+                            break;
+                        case 12:
+                            onHand= (int) cell.getNumericCellValue();
+                            break;
+                        case 13:
+                            sold=(int)cell.getNumericCellValue();
+                            break;
+                        case 14:
+                            Date=cell.getStringCellValue();
+                            shipment= LocalDate.parse(Date.substring(0,5)+"-"+Date.substring(5,8)+"-"+Date.substring(8));
+                            break;
+                        case 15:
+                            reserved=(int)cell.getNumericCellValue();
+                            break;
+                    }
+                    new Product(status,productID,description,vendor,onHand,sold,shipment,reserved);
+                }
+                if (counter>=300){
+                    break;
+                }
+                counter++;
+            }
+                } catch (FileNotFoundException e){
             System.out.println(e.getMessage());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
     }
 }
