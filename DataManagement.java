7d23dfa2b442c7902b924b3084c1bc7597f99627
kidxash <119833518+kidxash@users.noreply.github.com>
2024-07-21 21:06:25 -0400

    Update DataManagement.java

--- DataManagement.java
+++ DataManagement.java
@@ -4,5 +4,21 @@ import java.io.FileWriter;
 public class DataManagement {
     File customerFile=new File("Customer.txt");
 
+public void printFiles()
+    {
+        try{
+            Scanner scan  = new Scanner(customerFiles);
+            String files = scan.nextLine()
+                Customer.rawData()
+
+        }catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+
+
+            
+    }
+
+
+
 
 }
